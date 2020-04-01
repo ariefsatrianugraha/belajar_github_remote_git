2dd152b398641cb43482d8de02b7184648ade4a6
ariefsatrianugraha <developersatria@gmail.com>
2020-04-01 16:53:10 +0700

    menambahkan paragrap di index

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello Word</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium dicta ea at blanditiis sint pariatur soluta, exercitationem deleniti eveniet repudiandae aspernatur assumenda id laboriosam nostrum dolore expedita, voluptatibus omnis ad!</p>
 </body>
 </html>
